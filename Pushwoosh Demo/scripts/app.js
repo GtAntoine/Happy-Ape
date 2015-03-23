922f1e152f4a2d8981d92ac8429be675860afb74
GtAntoine <antoine.gt@orange.fr>
2015-03-23 13:57:56 +0100

    Update app.js

--- Pushwoosh Demo/scripts/app.js
+++ Pushwoosh Demo/scripts/app.js
@@ -1,4 +1,38 @@
 (function (global) {
+
+    var app = global.app = global.app || {};
+
+    // this function is called by Cordova when the application is loaded by the device
+    document.addEventListener('deviceready', function () {
+
+        // hide the splash screen as soon as the app is ready. otherwise
+        // Cordova will wait 5 very long seconds to do it for you.
+        navigator.splashscreen.hide();
+
+
+        app.application = new kendo.mobile.Application(document.body, {
+
+            // you can change the default transition (slide, zoom or fade)
+            transition: 'fade',
+
+            // comment out the following line to get a UI which matches the look
+            // and feel of the operating system
+            skin: 'flat',
+            statusBarStyle: "black-translucent"
+
+        });
+
+    }, false);
+
+})(window);
+
+
+function scroll() {
+    app.scroller().reset();
+}
+
+
+/*(function (global) {
     var app = global.app = global.app || {};
 
     document.addEventListener('deviceready', function () {
@@ -20,4 +54,4 @@
 
         app.application = new kendo.mobile.Application(document.body, { layout: "tabstrip-layout" });
     }, false);
-})(window);
+})(window);*/
